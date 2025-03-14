af5a558ce4350ee4713755723fafda20f0908553
dbengp <dbengp@hotmail.com>
2025-03-13 23:31:22 -0300

    content page created with dynamic loading

---
+++ blog/src/app/data/datafake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {   
+        id: "1",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXHpp8MVF1md7AxFVcvTL2z_7voOigWMiWJA&s",
+        title: "Content Title 1",
+        description: "Content Description 1"
+    },
+    {   
+        id: "2",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxiiYJMEEAjdQ7FiM-8YCGeuAWFHuC9Qmbmg&s",
+        title: "Content Title 2",
+        description: "Content Description 2"
+    },
+    {   
+        id: "3",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfPmQBna8bWHacXMA8s7n8r6sBI6Ave1FkSg&s",
+        title: "Content Title 3",
+        description: "Content Description 3"
+    },
+    {   
+        id: "4",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRntmuA_LMirlvS4fX9-H1gyI4NIIughOnWqg&s",
+        title: "Content Title 4",
+        description: "Content Description 4"
+    }
+]
